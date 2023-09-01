66e7742cdd94675e1eb844cf53d64db3b89d50de
Ruth Chanarin <chanarinruth@gmail.com>
2023-09-01 16:34:45 +0100

    Games
    Added a games folder to my repo, with hangman & fizzbuzz

---
+++ games/fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz(number: int = 21) -> list:
+    for x in range(1,number+1):
+        if x % 3 == 0 and x % 5 == 0:
+            print("fizzbuzz!")
+        elif x % 3 == 0:
+            print("fizz")
+        elif x % 5 == 0:
+            print("buzz")
+        else:
+            print(x)
+
